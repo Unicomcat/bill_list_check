c48250de9493ba75e9e56ed19617e0908670fbbe
unicomcat <unicomcat@outlook.com>
2016-05-25 16:55:40 +0800

    5.25

--- __init__.py
+++ __init__.py
@@ -2,6 +2,6 @@ __author__ = 'Unicomcat'
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from database import CG, BILLLIST
-
+from config import config
 Base = declarative_base()
-engin = create_engine()
+engin = create_engine(config)
--- config.py
+++ config.py
@@ -1,1 +1,21 @@
 __author__ = 'Unicomcat'
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    pass
+
+
+class DevelopmentConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
